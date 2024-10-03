4a6c3ecb17db92db63f947b9c46c1d299d2b8165
Abhishek kr <134193176+krAbhishek09@users.noreply.github.com>
2024-10-03 21:12:29 +0530

    Add files via upload

---
+++ A.py
@@ -0,0 +1,54 @@
+# a = float(input("enter 1st number"))
+# b = float(input("enter 2nd number "))
+# c = a+b
+# print("total number", c)
+
+
+# str="Abhishek"
+# print(len(str))
+
+# str ="I am codera"
+# print(str.count("a"))
+
+# lst = [10, 22, 45, 6, 1, 3, 4]  # Use square brackets for a list
+# a = len(lst)
+
+# for i in range(a):
+#     for j in range(i+1, a):
+#         if lst[i] > lst[j]:
+#             lst[i], lst[j] = lst[j], lst[i]  # Swap elements if the current is greater than the next
+
+# print("total number", lst)
+
+
+# n = 1212  # Change this to the number you want to check
+# n_str = str(n)
+# if n_str == n_str[::-1]:
+#     print("It is a palindrome")
+# else:
+#     print("It is not a palindrome")
+
+
+# list=[2,4,5,6,7,8,9,10,11,12]
+# a=len(list)
+# k=10
+# for i in range(a):
+#     for j in range (i+1,a):
+#         if list[i]+list[j]==k:
+#             print(list[i], list[j])
+
+# n = 10  # Number of terms in the series
+# a, b = 0, 1
+
+# print("Fibonacci Series:")
+# for _ in range(n):
+#     print(a, end=" ")
+#     a, b = b, a + b
+
+
+# n = 1
+# for i in range(100):
+#     if i % 2 == 0:
+#         if (i == 4 or i == 6):
+#             # continue
+#             print(i, end=" ")
